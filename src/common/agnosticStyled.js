d7bd530cdbd8fe705b946bc7eff4dc4115f21d9b
buiminh15 <mike.bui@techvify.com.vn>
2021-10-28 17:18:38 +0700

    add

---
+++ src/common/agnosticStyled.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default styled(
+  ({tag = 'div', children, ...props}) =>
+    React.createElement(tag, props, children)
+)
---
+++ src/containers/App/index.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+      Appaa
+    </div>
+  )
+}
+
+export default App
