27b58eff63393e9d4ccb0453be94d1b69cb15d3c
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2022-04-27 20:58:47 +0700

    init

---
+++ src/themes/Colors.js
@@ -0,0 +1,5 @@
+const COLORS = {
+  mainColor: '#FFCE40',
+  black: '#1B1C1F',
+  white: '#FFF',
+}
---
+++ src/themes/Images.js
@@ -0,0 +1,34 @@
+import logo from 'assets/icons/logo.svg'
+import rect from 'assets/icons/Rectangle.svg'
+import layer1 from 'assets/icons/Vectorlayer1.svg'
+import BadgeAppStore from 'assets/images/BadgeAppStore.png'
+import BadgeGoogle from 'assets/images/BadgeGoogle.png'
+import MainPhones from 'assets/images/MainPhones.png'
+
+const hbLogo = {
+  alt: 'logo',
+  src: logo
+}
+
+const hbRect = {
+  alt: 'rectangle',
+  src: rect
+}
+const hbLayer1 = {
+  alt: 'layer 1',
+  src: layer1
+}
+const hbBadgeAppStore = {
+  alt: 'BadgeAppStore',
+  src: BadgeAppStore
+}
+const hbBadgeGoogle = {
+  alt: 'BadgeGoogle',
+  src: BadgeGoogle
+}
+const hbMainPhones = {
+  alt: 'MainPhones',
+  src: MainPhones
+}
+
+export { hbLogo, hbRect, hbLayer1, hbBadgeAppStore, hbBadgeGoogle, hbMainPhones }
